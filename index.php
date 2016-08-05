3ac28ef7371aa92f89b7f2d51e9725d71bcc47af
Giuseppe Naponiello <beppenapo@arc-team.com>
2016-08-05 08:54:00 +0200

    implementazione stile

--- index.php
+++ index.php
@@ -5,15 +5,13 @@ require_once __DIR__ . '/class/facebook-sdk-v5/autoload.php';
 $fb = new Facebook\Facebook([
   'app_id' => '498077220384026',
   'app_secret' => '79ebf5d0be20a1fd44213ec63216f4e2',
-  'default_graph_version' => 'v2.5',
+  'default_graph_version' => 'v2.7',
 ]);
 
 
 $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email', 'public_profile']; // optional
 $loginUrl = $helper->getLoginUrl('http://localhost/callback.php', $permissions);
-
-echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 ?>
 
 
